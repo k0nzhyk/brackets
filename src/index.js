10b2432c1c5bae77f1f2b019164a03344c7426e7
k0nzhyk <79240381+k0nzhyk@users.noreply.github.com>
2021-03-16 01:58:57 +0600

    refactor: solving task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let array = [];
+
+       for( let i = 0; i < str.length; i++ ){
+           for ( let bracket of bracketsConfig ) {
+               if( str[i] == bracket[0]  && bracket[0] == bracket[1]
+                   && bracket[0] == array[array.length - 1] ){
+                   array.pop();
+                   break;
+               }
+               if ( str[i] == bracket[0] ){
+                   array.push( str[i] );
+                   break;
+               }
+               if( str[i] == bracket[1] ){
+                   if( bracket[0] != array.pop() ){
+                       return false;
+                   }
+               }
+           }
+       }
+       if( array.length ) return false;
+       return true;
+   }
